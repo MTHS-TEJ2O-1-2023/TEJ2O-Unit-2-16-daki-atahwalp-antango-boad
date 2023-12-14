3517495322b56bbb32820195c441d403b0d64af8
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-12-14 13:11:58 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,29 +12,25 @@ let distanceToObject: number = 0
 radio.setGroup(154)
 basic.showIcon(IconNames.Happy)
 
-// finding distance
-  basic.showIcon(IconNames.Happy)
+while (true) {
+  if (input.buttonIsPressed(Button.A) === true) {
+    // finding distance
+    basic.showIcon(IconNames.Happy)
     distanceToObject = sonar.ping(
       DigitalPin.P1,
       DigitalPin.P2,
       PingUnit.Centimeters
     )
-    basic.clearScreen()
-    basic.showNumber(distanceToObject)
-    basic.showString("cm")
-
-  while (true) {
-    if (input.buttonIsPressed(Button.A) === true) {
-      //sending message to other microbit
-      if (distanceToObject < 10) {
-        radio.sendString("!")
-       } else {
-         radio.sendString("*")
-       }
+    //sending message to other microbit
+    if (distanceToObject <= 10) {
+      radio.sendString('!')
+      } else {
+      radio.sendString('*')
     }
   }
+}
 // receiving messages
 radio.onReceivedString(function (recievedString) {
-  basic.clearScreen()
-  basic.showString(recievedString)
+    basic.clearScreen()
+    basic.showString(recievedString)
 })
